73f42fa65ce4d36213a8f6bac2f388b578d7cbfe
Argenis Razo <rgnsrazo@gmail.com>
2023-06-15 23:05:40 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import {puppyList} from './data.js'
+
+function App() {
+  console.log(puppyList)
+
+  return (
+    <>
+      <div>
+       
+      </div>
+      
+      <div className="card">
+       
+      </div>
+     
+    </>
+  )
+}
+
+export default App
