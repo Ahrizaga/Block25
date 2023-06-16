91b68f057c5693e8c6a32c430832cae0e2772b04
Argenis Razo <rgnsrazo@gmail.com>
2023-06-16 00:14:12 -0700

    puppy data with useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
+import { useState } from 'react'
 import './App.css'
 import {puppyList} from './data.js'
 
 function App() {
-  console.log(puppyList)
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log(puppies)
 
   return (
-    <>
-      <div>
-       
-      </div>
+    <div className="App">
+      {
+        puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>;
+        })
+        
+      }
+</div>
       
-      <div className="card">
-       
-      </div>
-     
-    </>
-  )
+  );
 }
 
 export default App
